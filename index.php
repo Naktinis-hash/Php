d6f37a31196a7002db452ca191571718ffc2c030
Naktinis-hash <57089674+Naktinis-hash@users.noreply.github.com>
2020-03-03 11:12:01 +0000

    bokalai

--- index.php
+++ index.php
@@ -1,6 +1,27 @@
 <?php
+$title = 'Bokalai';
 
 $money =rand(1, 15);
 $bokal_cost = 3;
 
 ?>
+
+<html>
+<head>
+    <title><?php print $title ?></title>
+</head>
+<style>
+    .bg-img {
+        width: 150px;
+        height: 150px;
+        background-size: cover;
+    }
+</style>
+<body>
+    <div class="bg-img">
+        <?php for (; $money > $bokal_cost; $money -= $bokal_cost):?>
+        <img src="https://dovanusalis.lt/image/cache/catalog/products/110/alaus-bokalas-stipruoliui-1-800x800.jpg" width="132px" alt="alus"/>
+        <?php endfor ;?>
+    </div>
+</body>
+</html>
