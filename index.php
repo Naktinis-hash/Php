959b7d87e39e3829b392a9e4511f485e9fc6a97b
Naktinis-hash <57089674+Naktinis-hash@users.noreply.github.com>
2020-03-04 12:38:36 +0000

    gta

--- index.php
+++ index.php
@@ -1,37 +1,64 @@
 <?php
-$ataskaita = [
-    [
-        'vardas' => 'Petras',
-        'pavarde' => 'Lizius',
-        'daiktai' =>[
-            [
-            'qty' => 1,
-            'item' => 'Toyota Prius',
-             'type' => 'car'
-            ],
-        ],
-    ],
-    [
-        'vardas' => 'Ona',
-        'pavarde' => 'Lazauskiene',
-        'daiktai'=> [
-            [
-            'kiekis' => 3,
-            'item' => 'malkos'
-            ],
-        ],
-    ],
-    [
-        'vardas' => 'Algirdas',
-        'pavarde' => 'Palutinskas',
-        'daiktai' =>[
-            [
-           'kiekis' => 3,
-           'item' => 'malkos'
-           ],
-        ],
-    ],
-];
 
-var_dump($ataskaita);
+
+$game = [
+    'time' => '12:08',
+    'player' => [
+        'armor' => 90,
+        'health' => 100,
+        'wanted_level' => 2,
+        'cash' => 3718892,
+        'position' => [
+            'x' => 100.123123,
+            'y' => 57.312313,
+            'z' => 5.212134
+        ],
+        'weapons' => [
+            'active_id' => 0,
+            'available' => [
+                [
+                    'name' => 'Dildo',
+                    'damage' => 30,
+                    'icon' => '....',
+                    'type' => 'meelee',
+                ],
+                [
+                    'name' => 'Uzi',
+                    'damage' => 70,
+                    'icon' => '....',
+                    'type' => 'firearm',
+                    'ammo' => [
+                        'magazine_size' => 150,
+                        'in_magazine' => 40,
+                        'total' => 900,
+                    ]
+                ]
+            ]
+        ],
+        'clothes' => [
+            'top' => [
+                'active_id' => null,
+                'available' => [
+                    [
+                        'name' => 'T-shirt',
+                        'model' => '....',
+                    ]
+                ]
+            ],
+            'bottom' => [
+                'active_id' => null,
+                'available' => [
+                    [
+                        'name' => 'Jeans',
+                        'model' => '....',
+                    ]
+                ]
+            ]
+        ]
+    ]
+];
+unset($game['player']['weapons']['available'][0]);
+$active_id = $game['player']['weapons']['active_id'];
+unset($game['player']['weapons']['available']['active_id']);
+var_dump($game);
 ?>
